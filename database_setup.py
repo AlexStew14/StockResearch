043feb729ce9a70d100d2cb1bd24f9354ab31aba
alex <alexstew6050@gmail.com>
2021-11-06 22:30:20 -0600

    Converting csv loading to be multiprocessor

--- database_setup.py
+++ database_setup.py
@@ -1,3 +1,6 @@
+from sqlalchemy.sql.expression import table
+
+
 def initialize_daily_table(engine):
     engine.execute("CREATE TABLE `daily` (\n\
     `date` datetime NOT NULL,\n\
@@ -27,70 +30,108 @@ def drop_table(engine, inspector, table_name):
     if inspector.has_table(table_name):
         engine.execute(f"DROP TABLE {table_name};")
 
-def read_csvs_into_daily(engine, inspector):
-    data_dir = os.getcwd()+'\\data'
+# def read_csvs_into_daily(engine, inspector):
+#     data_dir = os.getcwd()+'\\data'
 
-    if not inspector.has_table('daily'):
-        initialize_daily_table(engine)    
+#     if not inspector.has_table('daily'):
+#         initialize_daily_table(engine)    
 
-    aggregate_df = None
-    for i, filename in enumerate(os.listdir(data_dir)):
-        if filename[-3:] != 'csv':
-            continue
+#     aggregate_df = None
+#     for i, filename in enumerate(os.listdir(data_dir)):
+#         if filename[-3:] != 'csv':
+#             continue
 
-        file_path = os.path.join(data_dir, filename)
+#         file_path = os.path.join(data_dir, filename)
         
-        df = pd.read_csv(file_path)
-        df.date = df.date.astype('datetime64[D]')
-        df.drop(columns=['changeOverTime', 'label'], inplace=True)
-        df['ticker'] = filename.split('_')[0].upper()
-        df.replace([np.inf, -np.inf], np.nan, inplace=True)
-        df = df.dropna()
+#         df = pd.read_csv(file_path)
+#         df.date = df.date.astype('datetime64[D]')
+#         df.drop(columns=['changeOverTime', 'label'], inplace=True)
+#         df['ticker'] = filename.split('_')[0].upper()
+#         df.replace([np.inf, -np.inf], np.nan, inplace=True)
+#         df = df.dropna()
 
-        if aggregate_df is None:
-            aggregate_df = df.copy()
-        else:
-            aggregate_df = aggregate_df.append(df)
+#         if aggregate_df is None:
+#             aggregate_df = df.copy()
+#         else:
+#             aggregate_df = aggregate_df.append(df)
         
-        if df.shape[0] < 10:
-            continue
+#         if df.shape[0] < 10:
+#             continue
 
-        if i % 100 == 0:
-            aggregate_df.to_sql('temp_table', con=engine, if_exists='replace', index=False)
-            aggregate_df = None
-            print(str(i))
-            engine.execute('INSERT IGNORE INTO daily SELECT * FROM temp_table;')
-            engine.execute("DROP TABLE temp_table;")
+#         if i % 100 == 0:
+#             aggregate_df.to_sql('temp_table', con=engine, if_exists='replace', index=False)
+#             aggregate_df = None
+#             print(str(i))
+#             engine.execute('INSERT IGNORE INTO daily SELECT * FROM temp_table;')
+#             engine.execute("DROP TABLE temp_table;")
 
-    aggregate_df.to_sql('temp_table', con=engine, if_exists='replace', index=False)
-    print(str(i))
-    engine.execute('INSERT IGNORE INTO daily SELECT * FROM temp_table;')
-    engine.execute("DROP TABLE temp_table;")
+#     aggregate_df.to_sql('temp_table', con=engine, if_exists='replace', index=False)
+#     print(str(i))
+#     engine.execute('INSERT IGNORE INTO daily SELECT * FROM temp_table;')
+#     engine.execute("DROP TABLE temp_table;")
 
     
 
-def read_csv_into_tickers(engine, inspector):
-    if not inspector.has_table('tickers'):
-        initialize_tickers_table(engine)  
+# def read_csv_into_tickers(engine, inspector):
+#     if not inspector.has_table('tickers'):
+#         initialize_tickers_table(engine)  
 
-    data_dir = os.getcwd() +'\\data\\tickers'
+#     data_dir = os.getcwd() +'\\data\\tickers'
 
-    for filename in os.listdir(data_dir):
-        if filename[-3:] != 'csv':
-            continue
+#     for filename in os.listdir(data_dir):
+#         if filename[-3:] != 'csv':
+#             continue
         
-        file_path = os.path.join(data_dir, filename)        
-        rawdata = open(file_path, 'rb').read()
-        encoding = chardet.detect(rawdata)['encoding']
-        df = pd.read_csv(file_path, encoding=encoding)
-        df = df.drop(columns=['name', 'price', 'exchange'])
-        df = df.rename(columns={'symbol': 'ticker', 'exchangeShortName': 'exchange'})
-        df = df[df['exchange'].isin(['NYSE', 'NASDAQ', 'ETF'])]
+#         file_path = os.path.join(data_dir, filename)        
+#         rawdata = open(file_path, 'rb').read()
+#         encoding = chardet.detect(rawdata)['encoding']
+#         df = pd.read_csv(file_path, encoding=encoding)
+#         df = df.drop(columns=['name', 'price', 'exchange'])
+#         df = df.rename(columns={'symbol': 'ticker', 'exchangeShortName': 'exchange'})
+#         df = df[df['exchange'].isin(['NYSE', 'NASDAQ', 'ETF'])]
 
-        df.to_sql('temp_ticker_table', con=engine, if_exists='replace', index=False)
-        engine.execute('INSERT IGNORE INTO tickers SELECT * FROM temp_ticker_table;')
-        engine.execute("DROP TABLE temp_ticker_table;")
-  
+#         df.to_sql('temp_ticker_table', con=engine, if_exists='replace', index=False)
+#         engine.execute('INSERT IGNORE INTO tickers SELECT * FROM temp_ticker_table;')
+#         engine.execute("DROP TABLE temp_ticker_table;")
+
+def __read_csvs(csv_list, table_name, process_id):
+    import sqlalchemy as sqla
+    import pandas as pd
+    import os
+
+    engine = sqla.create_engine('mysql+pymysql://user:user@localhost:3306/stock_data', echo=False)    
+    inspector = sqla.inspect(engine)
+    # if not inspector.has_table(table_name):
+    #     return
+
+    # ADD PREPROCESSING BASED ON TABLE NAME
+    # CAN SET FUNCTION SO YOU DONT HAVE TO DO IFS DURING LOOP
+    
+    temp_table = f'temp_table_{process_id}'
+    for csv in csv_list:
+        df = pd.read_csv(csv)
+        file_name = os.path.split(csv_list[0])[-1]
+        ticker = file_name.split('_')[0]        
+        df['ticker'] = ticker
+        df.to_sql(temp_table, con=engine, index=False, if_exists='replace')
+        engine.execute(f"INSERT IGNORE INTO {table_name} SELECT * FROM {temp_table};")
+        engine.execute(f"DROP TABLE {temp_table};")
+
+
+def read_csvs_into_table(csv_dir, table_name, num_processes=1):
+    data_dir = os.path.join(os.getcwd(), os.path.abspath(csv_dir))
+    
+    csv_file_names = [os.path.join(data_dir, name) for name in os.listdir(data_dir) if name[-4:] == '.csv']
+    if num_processes > 1:
+        csv_per_process = len(csv_file_names) // num_processes
+        for i in range(num_processes):
+            csv_batch = csv_file_names[i * csv_per_process: (i+1) * csv_per_process]
+            process = mp.Process(target=__read_csvs, args=(csv_batch, table_name, i))
+            process.start()
+
+    else:
+        __read_csvs(csv_file_names, table_name, 0)
+
 
 def download_all_stock_csvs(engine, inspector, csv_type='ratios'):
     if inspector.has_table('tickers'):
@@ -109,9 +150,15 @@ if __name__ == '__main__':
     import sys
     import chardet
     import fmpcloud_interface as fmp    
+    import multiprocessing as mp
 
-    engine = sqla.create_engine('mysql+pymysql://user:user@localhost:3306/stock_data', echo=False)    
-    inspector = sqla.inspect(engine)
+    # engine = sqla.create_engine('mysql+pymysql://user:user@localhost:3306/stock_data', echo=False)    
+    # df = pd.read_csv('./data/ratios/A_quarterly_ratios_06_11_21.csv')
+    # df['ticker'] = 'A'
+    # print(df.info())
+    # df.to_sql(name='ratios',con=engine, index=False)
+
+    read_csvs_into_table('data/ratios', 'ratios', 10)
 
 
  
